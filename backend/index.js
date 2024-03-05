8b89b735581a454ee7d931d5310ec2d079111cbd
atul kumar singh <atulsingh9507@gmail.com>
2024-03-05 16:08:50 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,8 @@ const app = express()
 const port = 3000
 
 // Available Routes
-app.use('/app/auth', require('./routes/auth.js'))
-app.use('/app/notes', require('./routes/notes.js'))
+app.use('/api/auth', require('./routes/auth.js'))
+app.use('/api/notes', require('./routes/notes.js'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
