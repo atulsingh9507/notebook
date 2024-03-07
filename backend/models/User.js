f8d26684180c921f46727845739653d6eb290161
atul kumar singh <atulsingh9507@gmail.com>
2024-03-07 14:24:52 +0530

    creating login endpoint & sending auth token

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-   name:{
-    type: String,
-    required: true
-   },
+   
+   
    email:{
     type: String,
     required: true,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,8 +12,8 @@ const JWT_SECRET = 'atulisagoodboy';
 
 // create a user using:post "/api/auth/create user". no loging required
 
-router.post('/createUser', [
-    body('name').isLength({ min: 3 }),
+router.post('/login', [
+    
     body('email').isEmail(),
     body('password').isLength({ min: 3 }),
 ], async (req, res)=>{ 
@@ -51,6 +51,42 @@ const salt =await bcrypt.genSalt(10);
    // res.json(user)
    res.json({authtoken})
 } catch (error){
+    console.error(error.message);
+    res.status(500).send("some error occured");
+}
+})
+// Authenticate a user using:post "/api/auth/login". no loging required
+router.post('/login', [
+    body('email', 'enter a valid email').isEmail(),
+    body('password', 'password can not be blank').exists(),
+], async (req, res)=>{
+ // if there are errors,return bad request and the errors
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+ }
+
+  const {email, password}= req.body;
+  try {
+    let user =await User.findOne({email});
+    if (!user){
+        return res.status(400).json({error:"please login with correct"});
+
+    }
+
+  const passwordCompare =await bcrypt.compare(password, user.password);
+  if(!passwordcompare){
+    return res.status(400).json({error:"please login with correct"});
+  }
+  const data ={
+    user:{
+        id:user.id
+    }
+}
+const authtoken = jwt.sign(data, JWT_SECRET);
+res.json({authtoken})
+
+  } catch (error){
     console.error(error.message);
     res.status(500).send("some error occured");
 }
