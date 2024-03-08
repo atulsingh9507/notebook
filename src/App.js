dc0e37e4e7326f58a707c790c416b6290b5e8376
atul kumar singh <atulsingh9507@gmail.com>
2024-03-08 15:32:37 +0530

    creating navebar and routs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
-import './App.css';
+import './App.css'; 
+ import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+
+} from "react-router-dom";
+import Navebar from './components/Navebar';
+import Home from './components/Home';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>this is notebook</h1>
-    </div>
+    <>
+    <Router>
+    <Navebar/>
+    <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route exact path="/about" element={<About />} />
+        </Routes>
+        </Router>
+
+    </>
   );
 }
 
